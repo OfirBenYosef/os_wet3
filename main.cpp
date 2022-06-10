b00c554c94a612fb9e2411940cfe4873c74bca8e
Daniel Fux <danielfux@campus.technion.ac.il>
2022-06-10 17:32:04 +0300

    trial 1

--- main.cpp
+++ main.cpp
@@ -12,8 +12,16 @@
 #include <sys/select.h>
 #include <netinet/in.h>
 #include <fcntl.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
 unsigned int fail_counter;
+
+#define RESPONSE_MSG "I got your message”
+
+using namespace std;
 int main(int argc, char *argv[]) {
+
     if (argc != 4) {
         cout << "incorrect number of args" << endl;
         exit(1);
@@ -22,8 +30,8 @@ int main(int argc, char *argv[]) {
         cout << "incorrect number of args" << endl;
         exit(1);
     }
-
-    string port = argv[1];
+    char buffer[512] = {0};
+    unsigned short port = atoi(argv[1]);
     int timeout = atoi(argv[2]);
     int max_num_of_resends = atoi(argv[3]);
 
@@ -32,8 +40,8 @@ int main(int argc, char *argv[]) {
     time_val.tv_usec = 0;
 
     //create socket
-    int socket_ = socket(AF_INET, SOCK_DGRAM, 0);
-    if (socket_ < 0) {
+    int my_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (my_socket < 0) {
         perror("TTFTP_ERROR: socket() failed");
         exit(1);
     }
@@ -41,15 +49,34 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in clnt_addr;
 
     //initial out structures
-    memset(&ServAddr, 0, sizeof(echoServAddr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(echoServPort);
+    serv_addr.sin_port = htons(port);
 
     //bind to the address
-    if (bind(socket_, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+    //int a=bind(socket_, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    if ((bind(my_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)) {
         perror("TTFTP_ERROR: bind() failed");
         exit(1);
+    }
+    listen(my_socket,1);
+    unsigned int client_addr_len = sizeof(clnt_addr);
+    int newsockfd = accept(my_socket,(struct sockaddr *)&clnt_addr, &client_addr_len);
+    if (newsockfd < 0) perror("ERROR on accept");
+    int n = read(newsockfd, buffer, 512);
+
+    while(true) {
+
+        int cliAddrLen = sizeof(clnt_addr);
+        int recvMsgSize;
+        if ((recvMsgSize = recvfrom(my_socket, buffer, 512, 0, (struct sockaddr *) &clnt_addr,
+                                    reinterpret_cast<socklen_t *>(&cliAddrLen))) < 0)
+            perror("recvfrom() failed");
+        printf("Handling client %s\n",inet_ntoa(clnt_addr.sin_addr));
+
+        if (sendto(my_socket,buffer, recvMsgSize, 0,(struct sockaddr *) &clnt_addr,sizeof(clnt_addr)) != recvMsgSize)
+            perror("sendto() sent a different number of bytes than expected");
     }
 }
 
